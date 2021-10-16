1ca51f60b8bb480787069f4534b89d9f382163c5
Sujith Bonthala <78267490+SujithBonthala@users.noreply.github.com>
2021-10-16 13:11:33 +0530

    Update MD5_Algo_Client.c

--- MD5_Algo_Client.c
+++ MD5_Algo_Client.c
@@ -4,25 +4,22 @@ int main(int argc, char *argv[])
 {
     printf("\nFile Authentication using MD5\n");
     int choice;
-    do
+    printf("\n1. Create a MD5 hash to be stored in our database\n");
+    printf("2. Validate your downloaded file to the pre-existing hashes in our database\n");
+    printf("3. Exit\n");
+    printf("Enter your choice : ");
+    scanf("%d",&choice);
+    fflush(stdin);
+    switch(choice)
     {
-        printf("\n1. Create a MD5 hash to be stored in our database\n");
-        printf("2. Validate your downloaded file to the pre-existing hashes in our database\n");
-        printf("3. Exit\n");
-        printf("Enter your choice : ");
-        scanf("%d",&choice);
-        fflush(stdin);
-        switch(choice)
-        {
-            case 1:
-                storeHash();
-                break;
-            case 2:
-                validateFile();
-                break;
-            case 3:
-                break;
-        }
-    }while(choice<3);
+        case 1:
+            storeHash();
+            break;
+        case 2:
+            validateFile();
+            break;
+        case 3:
+        break;
+    }
     return 0;
 }
